b29686bb13ee38efe15b7c07bc5fd03b96f29c40
Wilfred Ruck <wilfredoruck@gmail.com>
2017-10-01 21:20:58 -0400

    new app

---
+++ app/controllers/code_controller.rb
@@ -0,0 +1,29 @@
+class CodeController < ApplicationController
+  def index
+  	@code = Code.last
+  	@codes = Code.all
+  end
+
+  def create
+  	code = ''
+  	while(code == '')
+	  	firstLetters = ('A'..'Z').to_a.shuffle[0,2].join
+	  	
+	  	numbers = rand(0...99).to_s
+	  	if numbers.length == 1
+	  		numbers.insert(0,'0')
+	  	end
+
+	  	secondLetters = ('A'..'Z').to_a.shuffle[0,2].join
+
+	  	code = (firstLetters + numbers + secondLetters)
+	  	if (Code.where(code: code).any? == false)
+		  	Code.create(code: code)
+	  		redirect_to root_path
+	  	else
+	  		code = ''
+	  	end
+	end
+  end
+
+end
